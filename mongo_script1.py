150c566666d2d4d0f54f4e78e80b2e7f33e98c3f
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-07-05 17:06:47 -0700

    Update mongo_script1.py

--- mongo_script1.py
+++ mongo_script1.py
@@ -30,6 +30,22 @@ for value in versions:
 for j in db.modulestore.structures.find({}, {"previous_version": 1}):
     """ This will give the list of Dictionary's containg _id and previous_version """
     available_version_list.append(j)
+available_version_list_with_prev_original = []
+
+available_version_list_with_id_val = []
+for j in db.modulestore.structures.find({}, {"previous_version": 1, "original_version": 1}):
+    available_version_list_with_prev_original.append(j)
+    # print j
+
+for j in db.modulestore.structures.find({}, {"_id": 1}):
+    available_version_list_with_id_val.append(j)
+# print available_version_listwith_id_val
+list_of_avail_id = []
+for d in available_version_list_with_id_val:
+    if '_id' in d:
+        list_of_avail_id.append(d['_id'])
+
+    
 for j in db.modulestore.structures.find({}, {"previous_version": 1, "original_version": 1}):
     """ Extracting the list of Dictionary's containg _id ,previous_version and original_version """
     available_version_list_with_prev_original.append(j)
@@ -119,5 +135,25 @@ def mongo_version_manager():
     #this will delete all the extra or unnecessary versions
     db.modulestore.structures.remove({'_id': {'$in': final_to_be_deleted_versions}})
 
+def mongo_verion_linker():
+    for each in available_version_list_with_prev_original:
+        if each["previous_version"] is None:
+            print "Hi None"
+        elif each["previous_version"] not in list_of_avail_id and each["previous_version"] is not None:
+            to_be_linked_version_id = []
+            = []
+            c = []
+            to_be_linked_version_id.append(each['_id'])
+            #b.append(each['previous_version'])
+            original_version_id.append(each['original_version'])
+            print a
+            print b
+            print c
+            # we are appending into the arrray and linking it, Since $in in mongo query is expecting list 
+            db.modulestore.structures.update({'_id': {'$in': to_be_linked_version_id}}, {'$set': {"previous_version": original_version_id[0]}})
+        else:
+            print "*"
+    
 
 mongo_version_manager()
+mongo_verion_linker()
